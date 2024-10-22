4e47b7123510b9c663eceacb28e7ad65a53470c1
CorruptClaw <sebastianhaag4@gmail.com>
2024-10-22 16:01:37 +0200

    Scenery

--- ExampleTestProject/Assets/Script/BallMovement.cs
+++ ExampleTestProject/Assets/Script/BallMovement.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-
+    public float Speed = 10f;
+    private Rigidbody2D rb;
+    private Vector2 screenBounds;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = new Vector2(0, -Speed);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < screenBounds.y)
+        {
+            Destroy(this.gameObject);
+        }
+
+
     }
 
 }
