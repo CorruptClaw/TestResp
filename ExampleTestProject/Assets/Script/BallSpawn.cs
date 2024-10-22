592241a7c4e98a8c32c536467498a2bb20df51df
CorruptClaw <sebastianhaag4@gmail.com>
2024-10-22 13:39:19 +0200

    Fixing and checking INput

--- ExampleTestProject/Assets/Script/BallSpawn.cs
+++ ExampleTestProject/Assets/Script/BallSpawn.cs
@@ -1,11 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BallSpawn : MonoBehaviour
 {
+    public GameObject[] Targets;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -13,4 +19,7 @@ public class BallSpawn : MonoBehaviour
     {
         
     }
+
+    
+
 }
