82c4a14f3fefc7eeaad54798edccc67a27c9500c
CorruptClaw <sebastianhaag4@gmail.com>
2024-10-22 12:58:22 +0200

    Material and Obj
    Added Materials, colors, and now experimenting on scripts and building

--- ExampleTestProject/Assets/Script/BallSpawn.cs
+++ ExampleTestProject/Assets/Script/BallSpawn.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class PlayerMovementTest : MonoBehaviour
+public class BallSpawn : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
