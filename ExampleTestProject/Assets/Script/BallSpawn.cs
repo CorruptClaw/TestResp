98485618871451817b20f02f58a2f1f7f61d4a08
CorruptClaw <sebastianhaag4@gmail.com>
2024-10-24 13:48:34 +0200

    Update

--- ExampleTestProject/Assets/Script/BallSpawn.cs
+++ ExampleTestProject/Assets/Script/BallSpawn.cs
@@ -8,12 +8,19 @@ public class BallSpawn : MonoBehaviour
     public List <GameObject> Targets;
     //public GameObject target;
 
+    public bool spawnPlenty;
 
-    
+    public bool spawnSingle;
+    public bool Blue;
+    public bool Yellow;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        SpawnTarget();
+
+        
 
     }
 
@@ -25,19 +32,43 @@ public class BallSpawn : MonoBehaviour
 
     private void SpawnTarget()
     {
-        if (Targets.Count > 0)
+        if (spawnPlenty == true)
         {
-            int randomIndex = Random.Range(0, Targets.Count);
 
-            Instantiate(Targets[randomIndex], transform.position, Quaternion.identity);
+            if (Targets.Count > 0)
+            {
+                int randomIndex = Random.Range(0, Targets.Count);
+
+                Instantiate(Targets[randomIndex], transform.position, Quaternion.identity);
+            }
+            else
+            {
+                Debug.Log("No ball obj assaigned to the list");
+            }
+
+        }
+        else if (spawnSingle == true && spawnPlenty == false)
+        {
+            if (Blue == true)
+            {
+                Instantiate(Targets[0], transform.position, Quaternion.identity);
+            }
+            else if (Yellow == true && Blue == false)
+            {
+                Instantiate(Targets[5], transform.position, Quaternion.identity);
+            }
+            
         }
         else
         {
-            Debug.Log("No ball obj assaigned to the list");
+            return;
         }
 
     }
+
+    private void TargetObjRef()
+    {
+        
+    }
     
-
-
 }
