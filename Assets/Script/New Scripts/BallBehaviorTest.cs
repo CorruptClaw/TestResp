f49bbe95f3540f2765e7933529ad834bd47621a1
CorruptClaw <sebastianhaag4@gmail.com>
2024-11-12 14:42:48 +0100

    Scene and scripts
    Fixed some on the scene and fixed the scripts

--- Assets/Script/New Scripts/BallBehaviorTest.cs
+++ Assets/Script/New Scripts/BallBehaviorTest.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class BallBehaviorTest : MonoBehaviour
 {
-    public string color;       // Color identifier for the ball (e.g., "Blue")
+    private Rigidbody2D rb;
+    public string ballColor;       // Color identifier for the ball (e.g., "Blue")
     public Vector2Int position; // Position in the grid (e.g., (0, 1))
+
+    public bool kinematicOn;
 
     public void Initialize(string color, Vector2Int position)
     {
-        this.color = color;
+        this.ballColor = color;
         this.position = position;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        if (kinematicOn)
+        {
+            rb.bodyType = RigidbodyType2D.Kinematic;
+        }
     }
 
     // Update is called once per frame
